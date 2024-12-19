e8481b94c8a8f113aa87d487b662696406959488
Guillerki <guillemnaba@gmail.com>
2024-12-19 19:39:47 +0100

    exercici 1

---
+++ src/pec3/pec3.js
@@ -0,0 +1,39 @@
+// Exercise 1
+export function fetchUser(id, callback) { 
+    if (id === 1) {
+        callback("Brendan Eich"); 
+        return true; 
+    } else if (id === 2) {
+        callback("Douglas Crockford"); 
+        return true; 
+    } else if (id === 3) {
+        callback("Jeremy Ashkenas"); 
+        return true; 
+    } 
+    return false; 
+}
+
+// Exercise 2
+export function getComments(fetchUser, fetchPosts, fetchComments) {
+
+}
+
+// Exercise 3
+export function fetchUserPromise(id) {
+
+}
+
+// Exercise 4
+export async function fetchAsyncData(asyncCallback) {
+
+}
+
+// Exercise 5
+export async function getCommentsAsync(fetchUser, fetchPosts, fetchComments) {
+
+}
+
+// Exercise 6 
+export function createTaskWorker() {
+
+}
